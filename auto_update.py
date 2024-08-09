f885d8f485118919653a403be6f1b5bea0257eb0
YZ <yzhou1612@gmail.com>
2024-08-09 10:34:27 +0800

    update auto

--- auto_update.py
+++ auto_update.py
@@ -7,13 +7,14 @@ sys.path.append('Array')
 
 
 
-def add_and_commit(file_name):
+def add_and_commit(Question_name):
+    file_name = os.path.abspath(Question_name)
     os.system(f"git add {file_name}")
-    os.system(f'git commit -m "Add solution for LeetCode problem: {file_name}"')
+    os.system(f'git commit -m "Add solution for LeetCode problem: {Question_name}"')
 
 
 if __name__ == "__main__":
-    file_name = input("Enter the file name: ")
-    file_name = os.path.abspath(file_name)
-    add_and_commit(file_name)
+    Question_name = input("Enter the Question_name: ")
+    Question_name = os.path.abspath(Question_name)
+    add_and_commit(Question_name)
     os.system("git push origin master")
